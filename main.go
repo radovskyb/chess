772746204131c6343b4539961c77f0102524b975
Benjamin Radovsky <radovskyb@gmail.com>
2017-01-07 19:27:18 +1100

    ...

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"os"
+	"time"
 )
 
+type Color uint8
+
 const (
-	Black = "b"
-	White = "w"
+	Black Color = 1 << iota
+	White
 
 	Pawn   = "P"
 	Knight = "N"
@@ -19,25 +20,30 @@ const (
 	King   = "K"
 )
 
-type Position struct {
+type Pos struct {
 	X, Y int
 }
 
 type Piece struct {
-	Name  string
-	Color string
+	Name string
+	Color
 }
 
 func (p *Piece) String() string {
-	return fmt.Sprintf("%2s%s", p.Color, p.Name)
+	color := "w"
+	if p.Color == Black {
+		color = "b"
+	}
+	return fmt.Sprintf("%2s%s", color, p.Name)
 }
 
 type Board struct {
-	Pieces map[Position]*Piece
+	Turn   Color // who's turn it is.
+	Pieces map[Pos]*Piece
 }
 
 func NewBoard() *Board {
-	pieces := map[Position]*Piece{
+	pieces := map[Pos]*Piece{
 		// White
 		{0, 0}: {Rook, White},
 		{1, 0}: {Knight, White},
@@ -58,67 +64,61 @@ func NewBoard() *Board {
 		{7, 7}: {Rook, Black},
 	}
 	for i := 0; i < 8; i++ {
-		pieces[Position{i, 1}] = &Piece{Pawn, White}
-		pieces[Position{i, 6}] = &Piece{Pawn, Black}
+		pieces[Pos{i, 1}] = &Piece{Pawn, White}
+		pieces[Pos{i, 6}] = &Piece{Pawn, Black}
 	}
 	return &Board{Pieces: pieces}
 }
 
-func (b *Board) Print(w io.Writer) {
+func (b *Board) Print() {
+	fmt.Print("\033[H\033[2J\n")
 	for i1 := 0; i1 < 8; i1++ {
 		for i2 := 0; i2 < 8; i2++ {
-			if piece, found := b.Pieces[Position{i2, 7 - i1}]; found {
-				fmt.Fprintf(w, "%s", piece)
+			if piece, found := b.Pieces[Pos{i2, 7 - i1}]; found {
+				fmt.Printf("%s", piece)
 				continue
 			}
-			fmt.Fprintf(w, "%3s", "--")
+			fmt.Printf("%3s", "--")
 		}
 		fmt.Println()
 	}
 }
 
-// later return effects of move.
-func (b *Board) Move(p1, p2 Position) error {
+func (b *Board) Move(p1, p2 Pos) error {
 	piece, found := b.Pieces[p1]
 	if !found {
-		return fmt.Errorf("no piece at position %v", p1)
+		return fmt.Errorf("no piece found at position %v", p1)
 	}
-	b.Pieces[p2] = piece
-	delete(b.Pieces, p1)
+	b.Pieces[p2] = piece // add to new pos
+	delete(b.Pieces, p1) // delete piece at old pos
 	return nil
 }
 
 func main() {
 	b := NewBoard()
-	b.Print(os.Stdout)
+	b.Print()
+	time.Sleep(time.Second)
 
-	if err := b.Move(
-		Position{1, 1},
-		Position{1, 2},
-	); err != nil {
+	if err := b.Move(Pos{1, 1}, Pos{1, 2}); err != nil {
 		log.Fatalln(err)
 	}
+	b.Print()
+	time.Sleep(time.Second)
 
-	fmt.Println()
-	b.Print(os.Stdout)
-
-	if err := b.Move(
-		Position{1, 2},
-		Position{1, 3},
-	); err != nil {
+	if err := b.Move(Pos{1, 2}, Pos{1, 3}); err != nil {
 		log.Fatalln(err)
 	}
+	b.Print()
+	time.Sleep(time.Second)
 
-	fmt.Println()
-	b.Print(os.Stdout)
-
-	if err := b.Move(
-		Position{2, 6},
-		Position{2, 5},
-	); err != nil {
+	if err := b.Move(Pos{2, 6}, Pos{2, 4}); err != nil {
 		log.Fatalln(err)
 	}
+	b.Print()
+	time.Sleep(time.Second)
 
-	fmt.Println()
-	b.Print(os.Stdout)
+	if err := b.Move(Pos{2, 4}, Pos{1, 3}); err != nil {
+		log.Fatalln(err)
+	}
+	b.Print()
 }
