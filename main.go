47e4d1a16c0ec8278014bab830cf7bb8a9b9ff61
Benjamin Radovsky <radovskyb@gmail.com>
2017-01-08 15:41:23 +1100

    started writing available positions function

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 )
 
 type Color uint8
@@ -29,6 +28,56 @@ type Piece struct {
 	Color
 }
 
+func (p *Piece) AvailablePositions(cur Pos) map[Pos]struct{} {
+	avail := make(map[Pos]struct{})
+	switch {
+	case p.Name == Pawn:
+		avail[Pos{cur.X, cur.Y + 1}] = struct{}{}
+	case p.Name == Knight:
+		avail[Pos{cur.X + 2, cur.Y + 1}] = struct{}{}
+		avail[Pos{cur.X - 2, cur.Y + 1}] = struct{}{}
+		avail[Pos{cur.X + 2, cur.Y - 1}] = struct{}{}
+		avail[Pos{cur.X - 2, cur.Y - 1}] = struct{}{}
+		avail[Pos{cur.X + 1, cur.Y + 2}] = struct{}{}
+		avail[Pos{cur.X - 1, cur.Y + 2}] = struct{}{}
+		avail[Pos{cur.X + 1, cur.Y - 2}] = struct{}{}
+		avail[Pos{cur.X - 1, cur.Y - 2}] = struct{}{}
+	case p.Name == Bishop || p.Name == Queen:
+		for x, y := cur.X, cur.Y; x < 8 && y < 8; x, y = x+1, y+1 {
+			avail[Pos{x, y}] = struct{}{}
+		}
+		for x, y := cur.X, cur.Y; x >= 0 && y < 8; x, y = x-1, y+1 {
+			avail[Pos{x, y}] = struct{}{}
+		}
+		for x, y := cur.X, cur.Y; x < 8 && y >= 0; x, y = x+1, y-1 {
+			avail[Pos{x, y}] = struct{}{}
+		}
+		for x, y := cur.X, cur.Y; x >= 0 && y >= 0; x, y = x-1, y-1 {
+			avail[Pos{x, y}] = struct{}{}
+		}
+		fallthrough
+	case p.Name == Rook || p.Name == Queen:
+		for x := cur.X; x < 8; x++ {
+			avail[Pos{x, cur.Y}] = struct{}{}
+		}
+		for x := cur.X; x >= 0; x-- {
+			avail[Pos{x, cur.Y}] = struct{}{}
+		}
+		for y := cur.Y; y < 8; y++ {
+			avail[Pos{cur.X, y}] = struct{}{}
+		}
+		for y := cur.Y; y >= 0; y-- {
+			avail[Pos{cur.X, y}] = struct{}{}
+		}
+	case p.Name == King:
+		avail[Pos{cur.X + 1, cur.Y}] = struct{}{}
+		avail[Pos{cur.X - 1, cur.Y}] = struct{}{}
+		avail[Pos{cur.X, cur.Y + 1}] = struct{}{}
+		avail[Pos{cur.X, cur.Y - 1}] = struct{}{}
+	}
+	return avail
+}
+
 func (p *Piece) String() string {
 	color := "w"
 	if p.Color == Black {
@@ -40,6 +89,8 @@ func (p *Piece) String() string {
 type Board struct {
 	Turn   Color // who's turn it is.
 	Pieces map[Pos]*Piece
+	// pieceToPos map[*Piece]Pos
+	// King Line Of Sights
 }
 
 func NewBoard() *Board {
@@ -91,42 +142,62 @@ func (b *Board) Move(p1, p2 Pos) error {
 	}
 	b.Pieces[p2] = piece // add to new pos
 	delete(b.Pieces, p1) // delete piece at old pos
-	b.Print()
-	time.Sleep(time.Second)
 	return nil
 }
 
 func main() {
 	b := NewBoard()
 	b.Print()
-	time.Sleep(time.Second)
 
-	// White 1
-	if err := b.Move(Pos{4, 1}, Pos{4, 3}); err != nil {
-		log.Fatalln(err)
+	piece, found := b.Pieces[Pos{3, 0}]
+	if !found {
+		log.Fatalln("no bishop found")
 	}
-	// Black 1
-	if err := b.Move(Pos{4, 6}, Pos{4, 4}); err != nil {
-		log.Fatalln(err)
-	}
-	// White 2
-	if err := b.Move(Pos{5, 0}, Pos{2, 3}); err != nil {
-		log.Fatalln(err)
-	}
-	// Black 2
-	if err := b.Move(Pos{1, 7}, Pos{2, 5}); err != nil {
-		log.Fatalln(err)
-	}
-	// White 3
-	if err := b.Move(Pos{3, 0}, Pos{7, 4}); err != nil {
-		log.Fatalln(err)
-	}
-	// Black 3
-	if err := b.Move(Pos{3, 6}, Pos{3, 5}); err != nil {
-		log.Fatalln(err)
-	}
-	// White 4
-	if err := b.Move(Pos{7, 4}, Pos{5, 6}); err != nil {
-		log.Fatalln(err)
-	}
+	// b.Move[Pos{2, 7}]
+	fmt.Println(piece.AvailablePositions(Pos{3, 0}))
 }
+
+// func FourMoveCheckmate(b *Board) {
+// 	// White 1
+// 	if err := b.Move(Pos{4, 1}, Pos{4, 3}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// Black 1
+// 	if err := b.Move(Pos{4, 6}, Pos{4, 4}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// White 2
+// 	if err := b.Move(Pos{5, 0}, Pos{2, 3}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// Black 2
+// 	if err := b.Move(Pos{1, 7}, Pos{2, 5}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// White 3
+// 	if err := b.Move(Pos{3, 0}, Pos{7, 4}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// Black 3
+// 	if err := b.Move(Pos{3, 6}, Pos{3, 5}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// 	// White 4
+// 	if err := b.Move(Pos{7, 4}, Pos{5, 6}); err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	time.Sleep(time.Second)
+// 	b.Print()
+// }
