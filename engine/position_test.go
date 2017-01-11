3e62ba4a7f9d9e627ad7d476c4dbb4d73f691c7f
Benjamin Radovsky <radovskyb@gmail.com>
2017-01-11 18:03:49 +1100

    get move positions test

--- engine/position_test.go
+++ engine/position_test.go
@@ -67,7 +67,8 @@ func TestDiagPositions(t *testing.T) {
 	for _, tc := range testCases {
 		positions := diagPositions(tc.cur)
 		if len(positions) != len(tc.expected) {
-			t.Errorf("expected len(positions) to be %d", len(tc.expected))
+			t.Errorf("expected len(positions) to be %d, got %d",
+				len(tc.expected), len(positions))
 		}
 		for _, pos := range tc.expected {
 			if _, found := positions[pos]; !found {
@@ -116,7 +117,89 @@ func TestLinePositions(t *testing.T) {
 	for _, tc := range testCases {
 		positions := linePositions(tc.cur)
 		if len(positions) != len(tc.expected) {
-			t.Errorf("expected len(positions) to be %d", len(tc.expected))
+			t.Errorf("expected len(positions) to be %d, got %d",
+				len(tc.expected), len(positions))
+		}
+		for _, pos := range tc.expected {
+			if _, found := positions[pos]; !found {
+				t.Errorf("expected to find pos %v", pos)
+			}
+		}
+	}
+}
+
+func TestGetMovePositions(t *testing.T) {
+	testCases := []struct {
+		piece    *Piece
+		cur      Pos
+		expected []Pos
+	}{
+		{&Piece{Color: White, Name: Pawn}, Pos{0, 2}, []Pos{{0, 3}}},
+		{&Piece{Color: Black, Name: Pawn}, Pos{0, 5}, []Pos{{0, 4}}},
+		{
+			&Piece{Color: White, Name: Pawn}, Pos{0, 1},
+			[]Pos{{0, 2}, {0, 3}},
+		},
+		{
+			&Piece{Color: Black, Name: Pawn}, Pos{0, 6},
+			[]Pos{{0, 5}, {0, 4}},
+		},
+		{
+			&Piece{Name: Rook}, Pos{0, 0},
+			[]Pos{
+				// Vertical Positions.
+				{0, 0}, {0, 1}, {0, 2}, {0, 3},
+				{0, 4}, {0, 5}, {0, 6}, {0, 7},
+				// Horizontal Positions.
+				{1, 0}, {2, 0}, {3, 0}, {4, 0},
+				{5, 0}, {6, 0}, {7, 0},
+			},
+		},
+		{
+			&Piece{Name: Knight}, Pos{3, 3},
+			[]Pos{
+				{1, 2}, {2, 1}, {1, 4}, {4, 1},
+				{2, 5}, {5, 2}, {4, 5}, {5, 4},
+			},
+		},
+		{
+			&Piece{Name: Bishop}, Pos{4, 4},
+			[]Pos{
+				{0, 0}, {1, 1}, {2, 2}, {3, 3},
+				{4, 4}, {5, 5}, {6, 6}, {7, 7},
+				{1, 7}, {7, 1}, {2, 6}, {6, 2},
+				{3, 5}, {5, 3},
+			},
+		},
+		{
+			&Piece{Name: Queen}, Pos{4, 4},
+			[]Pos{
+				// Digaonal Positions.
+				{0, 0}, {1, 1}, {2, 2}, {3, 3},
+				{4, 4}, {5, 5}, {6, 6}, {7, 7},
+				{1, 7}, {7, 1}, {2, 6}, {6, 2},
+				{3, 5}, {5, 3},
+				// Vertical Line Positions.
+				{0, 4}, {1, 4}, {2, 4}, {3, 4},
+				{5, 4}, {6, 4}, {7, 4},
+				// Horizontal Line Positions.
+				{4, 0}, {4, 1}, {4, 2}, {4, 3},
+				{4, 5}, {4, 6}, {4, 7},
+			},
+		},
+		{
+			&Piece{Name: King}, Pos{2, 2},
+			[]Pos{
+				{1, 1}, {3, 3}, {2, 3}, {2, 1},
+				{1, 2}, {3, 2}, {1, 3}, {3, 1},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		positions := getMovePositions(tc.piece, tc.cur)
+		if len(positions) != len(tc.expected) {
+			t.Errorf("expected len(positions) to be %d, got %d",
+				len(tc.expected), len(positions))
 		}
 		for _, pos := range tc.expected {
 			if _, found := positions[pos]; !found {
