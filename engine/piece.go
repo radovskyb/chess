a26649556450d5b24551a30ec172adf22033126d
Benjamin Radovsky <radovskyb@gmail.com>
2017-01-13 22:06:47 +1100

    piece name to string

--- engine/piece.go
+++ engine/piece.go
@@ -13,6 +13,25 @@ const (
 	King
 )
 
+func (n PieceName) String() string {
+	switch n {
+	case Pawn:
+		return "pawn"
+	case Knight:
+		return "knight"
+	case Bishop:
+		return "bishop"
+	case Rook:
+		return "rook"
+	case Queen:
+		return "queen"
+	case King:
+		return "king"
+	default:
+		return "invalid piece name"
+	}
+}
+
 var pieceNames = map[PieceName]string{
 	Pawn: "\u2659", Knight: "\u2658", Bishop: "\u2657",
 	Rook: "\u2656", Queen: "\u2655", King: "\u2654",
